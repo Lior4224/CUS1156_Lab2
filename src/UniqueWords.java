7b0b77316952badb94abfbf4fc4405b70521273d
Lior4224 <polischouklior@gmail.com>
2024-09-29 17:14:11 -0400

    Finished countUnique method

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,3 +1,10 @@
+/*Project: Lab2
+ * Class: UniqueWords.java
+ * Author: Lior Polischouk
+ * Date: 9/29/2024
+ * Our task was to finish the countUnique method to return the count of unique Strings in an ArrayList
+ */
+
 import java.util.ArrayList;
 
 public class UniqueWords
@@ -10,12 +17,19 @@ public class UniqueWords
    public static int countUnique(ArrayList<String> list)
    {
 	  int count = 0;
+	  boolean unique = true;
 	  
       for (int i = 0; i < list.size(); i++)
       {		 for (int j = 0; j < list.size(); j++)
 		 {
-			
+			if (list.get(i).equals(list.get(j))) { // if element i is equal to any element of the array, unique = false
+				unique = false;
+				break;
+			}
 		 }
+	      if (unique) { // if unique is still true, that means element i was not equal to any other element, and count increases by 1
+	    	  count++;
+	      }
       }
 	  return count;
    }
